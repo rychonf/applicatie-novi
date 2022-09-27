d2faefd509b56f0a8f60da964bfa99b501b26bb9
rychonf <rychonf@hotmail.com>
2022-09-27 15:34:14 +0200

    Initial commit

---
+++ src/main/java/com/example/turnitup/TurnItUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.turnitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnItUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurnItUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/turnitup/TurnItUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turnitup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnItUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
