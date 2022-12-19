f4f3f9826e46434c3710f58e53afac6cc743b275
rychonf <rychonf@hotmail.com>
2022-12-19 17:10:19 +0100

    made query to find an organisation by name

--- src/main/java/com/example/turnitup/Repository/OrganisationRepository.java
+++ src/main/java/com/example/turnitup/Repository/OrganisationRepository.java
@@ -4,7 +4,10 @@ import com.example.turnitup.Model.Organisation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrganisationRepository extends JpaRepository<Organisation, Long> {
 
+    Optional <Organisation> findOrganisationByName(String organisationName);
 }
