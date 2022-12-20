86f4aaf2abe2ee2a488689bcfd26e93551c16f95
rychonf <rychonf@hotmail.com>
2022-12-20 14:01:50 +0100

    added parameter message

--- src/main/java/com/example/turnitup/Exception/BadRequestException.java
+++ src/main/java/com/example/turnitup/Exception/BadRequestException.java
@@ -3,7 +3,7 @@ package com.example.turnitup.Exception;
 public class BadRequestException extends RuntimeException{
     private static final Long serialVersionUID = 1L;
 
-    public BadRequestException(){
-        super();
+    public BadRequestException(String message){
+        super(message);
     }
 }
