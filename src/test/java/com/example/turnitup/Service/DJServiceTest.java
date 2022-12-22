b54b79bad8162d331d24badf97d338e214a61083
rychonf <rychonf@hotmail.com>
2022-12-22 23:57:00 +0100

    Created test for DJService

---
+++ src/test/java/com/example/turnitup/Service/DJServiceTest.java
@@ -0,0 +1,41 @@
+package com.example.turnitup.Service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DJServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllDJs() {
+    }
+
+    @Test
+    void getDJById() {
+    }
+
+    @Test
+    void createDJ() {
+    }
+
+    @Test
+    void updateDJ() {
+    }
+
+    @Test
+    void deleteDJById() {
+    }
+}
